e78e3b2f666403f2eb22683e73bcdd11a3e19c88
Elena <ebromeo88@gmail.com>
2023-02-22 21:42:04 +0100

    Modify app component

--- src/core/components/app/App.tsx
+++ src/core/components/app/App.tsx
@@ -1,5 +1,15 @@
+import { Actions } from "../../../features/components/actions/actions";
+import { Keyboard } from "../../../features/components/keyboard/keyboard";
+
 export function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <main className="phone">
+        <Keyboard></Keyboard>
+        <Actions></Actions>
+      </main>
+    </div>
+  );
 }
 
 export default App;
