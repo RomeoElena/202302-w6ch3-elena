8a1a352f7b36b6347596aad324f3857174cf4816
Elena <ebromeo88@gmail.com>
2023-02-22 21:42:34 +0100

    Add actions component

---
+++ src/features/components/actions/actions.tsx
@@ -0,0 +1,17 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../../core/store/store";
+
+export function Actions() {
+  const numbers = useSelector((state: RootState) => state.phone);
+  return (
+    <div className="actions">
+      <span className="number">{numbers}</span>
+      <a href="#" className="call">
+        Call
+      </a>
+      <a href="#" className="hang active">
+        Hang
+      </a>
+    </div>
+  );
+}
